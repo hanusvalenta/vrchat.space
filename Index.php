108d49bdab73ac300c353c9c4cada45bd14195b8
Hanuš Valenta <hanus.valenta@protonmail.com>
2022-08-08 02:23:29 +0200

    Initial commit

---
+++ Index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/Main.css">
+    <link rel="shortcut icon" href="images/ServerLogoTrans.ico">
+    <title>Welcome to VR</title>
+</head>
+<body>
+    <div class="All">
+        <div class="PFPS">
+            <h2>Special thanks to boosters</h2>
+            <img src="Images/ToxwaT.gif" width = auto height = "100">
+            <img src="Images/Paloid.png" width = auto height = "100">
+            <img src="Images/Kaiser Franz.png" width = auto height = "100">
+            <img src="Images/Daszerte.png" width = auto height = "100">
+            <img src="Images/Dami.gif" width = auto height = "100">
+            <img src="Images/Calmey.gif" width = auto height = "100">
+        </div>
+    
+
+        <div class=MainPillar>
+            <h1>Největší CZ/SK VRChat komunita</h1><br>
+
+            <div class="Buttons">
+                <a href="https://discord.gg/7nBhzhZaKy"><img alt="" src="images/DiscordButton.png"></a><br>
+                <a href="Socials.php"><img alt="" src="images/SocialsButton.png"></a><br>
+                <a href="Rules.php"><img alt="" src="images/RulesButton.png"></a><br>
+                <a href="Staff.php"><img alt="" src="images/StaffButton.png"></a><br>
+            </div>
+
+            <h2>Galery</h2>
+            <div class="PhotoScreen">
+                <img src="Images/People1.png" width = auto height = "300">
+                <img src="Images/People2.png" width = auto height = "300">
+                <img src="Images/People3.png" width = auto height = "300">
+                <img src="Images/People4.png" width = auto height = "300">
+                <img src="Images/People5.png" width = auto height = "300">
+                <img src="Images/People6.png" width = auto height = "300">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ Rules.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/Rules.css">
+    <link rel="shortcut icon" href="images/ServerLogoTrans.ico">
+    <title>Rules</title>
+</head>
+<body>
+    <div class="Main">
+        <h1>Pravidla</h1>
+        <p>Vím, že pravidla si nikdo moc nečte proto jsem se snažil to udělat co nejvíce lehce. Pokud se ti něco nelíbí, můžeš vždycky napsat někomu z vedení.</p>
+        <h2>CHOVEJ SE</h2>
+        <p>Nenadávej ostatním, nepiš nevhodné věci a nespamuj. Prezentuješ zde sám sebe a nikoho jiného, tak se prosím chovej.</p>
+        <h2>NSFW</h2>
+        <p>Neposílej nevhodné obrázky, videa, gify, odkazy a ani nepoužívejte nevhodné profilovky. Vím je to frustrující, ale pravidla se musí dodržovat.</p>
+        <h2>PROPAGACE</h2>
+        <p>Nepropaguj se jinde než v povolené kategorii a ani nepropaguj tento server nikde jinde, kde to není dovoleno. A už vůbec přes uživatele serveru pokud to sami nechtějí.</p>
+        <h2>NEPORUŠUJ PRAVIDLA DISCORDU</h2>
+        <p>I discord má svoje pravidla, které se musí dodržovat. Neznalost těchto pravidel se neomlouvá. (stačí znát základní)</p>
+        <h2>SCAMMING</h2>
+        <p>Nescamuj ostatní v dms a už vůbec ne na serveru.</p>
+        <h2>PORUŠENÍ</h2>
+        <p>Porušení vede k trestu a obcházení pravidel vede k přímému zabanování.</p>
+    </div>
+</body>
+</html>
