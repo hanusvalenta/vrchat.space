4b6d993cd65f82baad20654c7c81dbe5319238a5
Hanuš Valenta <hanus.valenta@protonmail.com>
2022-08-31 20:49:32 +0200

    fixes

--- Socials.php
+++ Socials.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/Socials">
+    <link rel="stylesheet" href="css/Socials.css">
     <link rel="shortcut icon" href="images/ServerLogoTrans.ico">
     <title>Socials</title>
 </head>
 <body>
-    
+    <p>test</p>
 </body>
 </html>
