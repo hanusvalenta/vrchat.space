2ca9686bca00ce3ad857f3f9cbce87c0b42116b0
Hanuš Valenta <hanus.valenta@protonmail.com>
2022-09-02 00:07:20 +0200

    scraped buttons

--- default.php
+++ default.php
@@ -11,40 +11,42 @@
 </head>
 <body>
     <div class="All">
-        <div class="Boosters">
-            <h2>Special thanks to boosters</h2>
-
-            <div class="BSTPFP">
-                <img src="Images/ToxwaT.gif" width = auto height = "100">
-                <img src="Images/Paloid.png" width = auto height = "100">
-                <img src="Images/Franz.png" width = auto height = "100">
-                <img src="Images/Daszerte.png" width = auto height = "100">
-                <img src="Images/Dami.gif" width = auto height = "100">
-                <img src="Images/Calmey.gif" width = auto height = "100">
-                <img src="Images/VardaCZ.png" width = auto height = "100">
-            </div>
-        </div>
-    
+        
 
         <div class=MainPillar>
             <h1>Největší CZ/SK VRChat komunita</h1><br>
 
             <div class="Buttons">
-                <a href="https://discord.gg/7nBhzhZaKy"><img alt="" src="Images/DiscordButton.png"></a><br>
-                <a href="Socials.php"><img alt="" src="Images/SocialsButton.png"></a><br>
-                <a href="Rules.php"><img alt="" src="Images/RulesButton.png"></a><br>
-                <a href="Staff.php"><img alt="" src="Images/StaffButton.png"></a><br>
+                <a href="https://discord.gg/7nBhzhZaKy"><button class="button bluebutton">Discord</button></a>
+                <a href="Socials.php"><button class="button normalbutton">Socials</button></a>
+                <a href="Galery.php"><button class="button normalbutton">Galery</button></a>
+                <a href="Rules.php"><button class="button normalbutton">Rules</button></a>
+                <a href="Staff.php"><button class="button normalbutton">Staff</button></a>
             </div>
 
             <div class="Chat">
-            <widgetbot
-            server="795076127961776149"
-            channel="795077211560214649"
-            
-            width="1000"
-            height="800"
-            ></widgetbot>
-            <script src="https://cdn.jsdelivr.net/npm/@widgetbot/html-embed"></script>
+                <widgetbot
+                server="795076127961776149"
+                channel="795077211560214649"
+                
+                width="1000"
+                height="800"
+                ></widgetbot>
+                <script src="https://cdn.jsdelivr.net/npm/@widgetbot/html-embed"></script>
+            </div>
+
+            <div class="Boosters">
+                <h2>Special thanks to boosters</h2>
+
+                <div class="BSTPFP">
+                    <img src="Images/ToxwaT.gif" width = auto height = "100">
+                    <img src="Images/Paloid.png" width = auto height = "100">
+                    <img src="Images/Franz.png" width = auto height = "100">
+                    <img src="Images/Daszerte.png" width = auto height = "100">
+                    <img src="Images/Dami.gif" width = auto height = "100">
+                    <img src="Images/Calmey.gif" width = auto height = "100">
+                    <img src="Images/VardaCZ.png" width = auto height = "100">
+                </div>
             </div>
         </div>
     </div>
