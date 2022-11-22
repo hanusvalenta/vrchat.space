13d591e22a5c132d21bd43299eac7c11a51a3059
Hanuš Valenta <hanus.valenta@protonmail.com>
2022-11-22 21:15:02 +0100

    added lightbox & new supporters avatars

--- default.php
+++ default.php
@@ -54,13 +54,17 @@
                 <h2>Special thanks to boosters</h2>
 
                 <div class="BSTPFP">
-                    <img src="Images/ToxwaT.gif" width = auto height = "100">
-                    <img src="Images/Paloid.png" width = auto height = "100">
-                    <img src="Images/Franz.png" width = auto height = "100">
-                    <img src="Images/Daszerte.png" width = auto height = "100">
-                    <img src="Images/Dami.gif" width = auto height = "100">
-                    <img src="Images/Calmey.gif" width = auto height = "100">
-                    <img src="Images/VardaCZ.png" width = auto height = "100">
+                    <img src="https://cdn.discordapp.com/avatars/416262848067534848/66258a0abb4c8a6eee9b319d8e836a55.png?size=1024" width = auto height = "100">
+                    <img src="https://cdn.discordapp.com/avatars/277500100144660481/a_efad45acb9da842060572be0c6eada71.gif?size=1024" width = auto height = "100">
+                    <img src="https://cdn.discordapp.com/avatars/288405408186630145/daab6ce7df40f3acfb4bf17a7c065c36.png?size=1024" width = auto height = "100">
+                    <img src="https://cdn.discordapp.com/avatars/161425741739851776/a_4eb2875d80b313fd89c3ba063baa4ac3.gif?size=1024" width = auto height = "100">
+                    <img src="https://cdn.discordapp.com/avatars/168274427480375296/98b40cd36c3241ec12a7a6fe4fe400ea.png?size=1024" width = auto height = "100">
+                    <img src="https://cdn.discordapp.com/avatars/704105166085029941/2a2381badb8671a50ed7915165b1a330.png?size=1024" width = auto height = "100">
+                    <img src="https://cdn.discordapp.com/avatars/573170183389315072/a_2d2f25551fabbd7897269ef85c16d441.gif?size=1024" width = auto height = "100">
+                    <img src="https://cdn.discordapp.com/avatars/696080545871822886/1c6b1bb1096c97508e81a7d4e9895921.png?size=1024" width = auto height = "100">
+                    <img src="https://cdn.discordapp.com/avatars/214486813069410304/a_1ca389238d70a1dfc1ec2a82d2c54adc.gif?size=1024" width = auto height = "100">
+                    <img src="https://cdn.discordapp.com/avatars/817487766778150983/da67d9f0092472196eed34f5d1d8a072.png?size=1024" width = auto height = "100">
+                    <img src="https://cdn.discordapp.com/avatars/544046014282203137/93b402535bdeb7f19860bd62cec85370.png?size=1024" width = auto height = "100">
                 </div>
             </div>
         </div>
