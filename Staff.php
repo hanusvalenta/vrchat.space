7db80021eef64c883f2cd2faa7073bcacac93073
Hanuš Valenta <hanus.valenta@protonmail.com>
2022-11-27 19:03:55 +0100

    New staff changes & boosters scrollbar

--- Staff.php
+++ Staff.php
@@ -6,6 +6,7 @@
     background-image: url("Images/Background.jpeg");
     }
     </style>
+    <link href="node_modules/lightbox2/src/css/lightbox.css" rel="stylesheet" />
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,6 +15,99 @@
     <title>Staff</title>
 </head>
 <body>
-    <h1>Test</h1>
+    <h1>Majitel:</h1>
+    <div class="StaffShowcase">
+        <div class="Staff">
+            <a href="Images/Staff/Paloid.png" data-lightbox="Staff" data-title="Paloid">
+                <img src="Images/Staff/Paloid.png" width = auto height = "100">
+            </a>
+            <div class="StaffInfo">
+                <h2>Paloid</h2>
+                <p>Nejvyšší a sám stvořitel serveru.</p>
+                <p>CZ</p>
+            </div>
+        </div>
+    </div>
+    <h1>Moderátoři:</h1>
+    <div class="StaffShowcase">
+        <div class="Staff">
+            <a href="" data-lightbox="Staff" data-title="">
+                <img src="" width = auto height = "100">
+            </a>
+            <div class="StaffInfo">
+                <h2></h2>
+                <p>Momentálně je pozice prázná</p>
+                <p></p>
+            </div>
+        </div>
+    </div>
+    <h1>Admini:</h1>
+    <div class="StaffShowcase">
+        <div class="Staff">
+            <a href="Images/Staff/Anoth.png" data-lightbox="Staff" data-title="Ultimate Hentai Master">
+                <img src="Images/Staff/Anoth.png" width = auto height = "100">
+            </a>
+            <div class="StaffInfo">
+                <h2>AnotherFunnyNickname</h2>
+                <p>Official and best Vrchat CZ/SK customer line</p>
+                <p>CZ</p>
+            </div>
+        </div>
+        <div class="Staff">
+            <a href="Images/Staff/Nero.png" data-lightbox="Staff" data-title="Nero">
+                <img src="Images/Staff/Nero.png" width = auto height = "100">
+            </a>
+            <div class="StaffInfo">
+                <h2>Nero</h2>
+                <p>Programmer, mostly C# and web</p>
+                <p>VRC avatar maker</p>
+                <p>SK</p>
+            </div>
+        </div>
+        <div class="Staff">
+            <a href="Images/Staff/Calmey.gif" data-lightbox="Staff" data-title="Calmey">
+                <img src="Images/Staff/Calmey.gif" width = auto height = "100">
+            </a>
+            <div class="StaffInfo">
+                <h2>Calmey</h2>
+                <p>i am just trying to survive all this fdup things so don't force me in to anything thx</p>
+                <p>CZ</p>
+            </div>
+        </div>
+        <div class="Staff">
+            <a href="Images/Staff/Zorky.png" data-lightbox="Staff" data-title="Zorky">
+                <img src="Images/Staff/Zorky.png" width = auto height = "100">
+            </a>
+            <div class="StaffInfo">
+                <h2>Zorky</h2>
+                <p>✨️🐾ayoo🐾✨️</p>
+                <p>SK</p>
+            </div>
+        </div>
+    </div>
+    <h1>Eventeři:</h1>
+    <div class="StaffShowcase">
+        <div class="Staff">
+            <a href="Images/Staff/Franz.png" data-lightbox="Staff" data-title="Kaiser">
+                <img src="Images/Staff/Franz.png" width = auto height = "100">
+            </a>
+            <div class="StaffInfo">
+                <h2>Kaiser</h2>
+                <p>Amateur weeb DJ</p>
+                <p>CZ</p>
+            </div>
+        </div>
+        <div class="Staff">
+            <a href="Images/Staff/Adamage.png" data-lightbox="Staff" data-title="">
+                <img src="Images/Staff/Adamage.png" width = auto height = "100">
+            </a>
+            <div class="StaffInfo">
+                <h2>Adamage</h2>
+                <p>VRchat: Adamage</p>
+                <p>SK</p>
+            </div>
+        </div>
+    </div>
+    <script src="node_modules/lightbox2/dist/js/lightbox-plus-jquery.js"></script>
 </body>
 </html>
