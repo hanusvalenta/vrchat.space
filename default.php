d3bc1c15af48f22fcff2d68f77c962382e7bf3da
Hanuš Valenta <hanus.valenta@protonmail.com>
2022-11-27 15:10:19 +0100

    New booster system

--- default.php
+++ default.php
@@ -58,45 +58,52 @@
                 <h2>Zvláštní poděkování boosterům</h2>
 
                 <div class="BSTPFP">
-                    <a href="https://cdn.discordapp.com/avatars/416262848067534848/66258a0abb4c8a6eee9b319d8e836a55.png?size=1024" data-lightbox="Boosters">
-                        <img src="https://cdn.discordapp.com/avatars/416262848067534848/66258a0abb4c8a6eee9b319d8e836a55.png?size=1024" width = auto height = "100">
+                    <a href="Images/Boosters/Benny.gif" data-lightbox="Boosters" data-title="Benny">
+                        <img src="Images/Boosters/Benny.gif" width = auto height = "100">
                     </a>
-                    <a href="https://cdn.discordapp.com/avatars/277500100144660481/a_efad45acb9da842060572be0c6eada71.gif?size=1024" data-lightbox="Boosters">
-                        <img src="https://cdn.discordapp.com/avatars/277500100144660481/a_efad45acb9da842060572be0c6eada71.gif?size=1024" width = auto height = "100">
+                    <a href="Images/Boosters/Calmey.gif" data-lightbox="Boosters" data-title="Calmey">
+                        <img src="Images/Boosters/Calmey.gif" width = auto height = "100">
                     </a>
-                    <a href="https://cdn.discordapp.com/avatars/288405408186630145/daab6ce7df40f3acfb4bf17a7c065c36.png?size=1024" data-lightbox="Boosters">
-                        <img src="https://cdn.discordapp.com/avatars/288405408186630145/daab6ce7df40f3acfb4bf17a7c065c36.png?size=1024" width = auto height = "100">
+                    <a href="Images/Boosters/coffinkat.png" data-lightbox="Boosters" data-title="Coffincat">
+                        <img src="Images/Boosters/coffinkat.png" width = auto height = "100">
                     </a>
-                    <a href="https://cdn.discordapp.com/avatars/161425741739851776/a_4eb2875d80b313fd89c3ba063baa4ac3.gif?size=1024" data-lightbox="Boosters">
-                        <img src="https://cdn.discordapp.com/avatars/161425741739851776/a_4eb2875d80b313fd89c3ba063baa4ac3.gif?size=1024" width = auto height = "100">
+                    <a href="Images/Boosters/Daszerte.png" data-lightbox="Boosters" data-title="Daszerte">
+                        <img src="Images/Boosters/Daszerte.png" width = auto height = "100">
                     </a>
-                    <a href="https://cdn.discordapp.com/avatars/168274427480375296/98b40cd36c3241ec12a7a6fe4fe400ea.png?size=1024" data-lightbox="Boosters">
-                        <img src="https://cdn.discordapp.com/avatars/168274427480375296/98b40cd36c3241ec12a7a6fe4fe400ea.png?size=1024" width = auto height = "100">
+                    <a href="Images/Boosters/Evie.png" data-lightbox="Boosters" data-title="Evie">
+                        <img src="Images/Boosters/Evie.png" width = auto height = "100">
                     </a>
-                    <a href="https://cdn.discordapp.com/avatars/704105166085029941/2a2381badb8671a50ed7915165b1a330.png?size=1024" data-lightbox="Boosters">
-                        <img src="https://cdn.discordapp.com/avatars/704105166085029941/2a2381badb8671a50ed7915165b1a330.png?size=1024" width = auto height = "100">
+                    <a href="Images/Boosters/GLOWE3Y.gif" data-lightbox="Boosters" data-title="Glowe3y">
+                        <img src="Images/Boosters/GLOWE3Y.gif" width = auto height = "100">
                     </a>
-                    <a href="https://cdn.discordapp.com/avatars/573170183389315072/a_2d2f25551fabbd7897269ef85c16d441.gif?size=1024" data-lightbox="Boosters">
-                        <img src="https://cdn.discordapp.com/avatars/573170183389315072/a_2d2f25551fabbd7897269ef85c16d441.gif?size=1024" width = auto height = "100">
+                    <a href="Images/Boosters/Goald.png" data-lightbox="Boosters" data-title="Goald">
+                        <img src="Images/Boosters/Goald.png" width = auto height = "100">
                     </a>
-                    <a href="https://cdn.discordapp.com/avatars/696080545871822886/1c6b1bb1096c97508e81a7d4e9895921.png?size=1024" data-lightbox="Boosters">
-                        <img src="https://cdn.discordapp.com/avatars/696080545871822886/1c6b1bb1096c97508e81a7d4e9895921.png?size=1024" width = auto height = "100">
+                    <a href="Images/Boosters/Kaiser.png" data-lightbox="Boosters" data-title="Kaiser">
+                        <img src="Images/Boosters/Kaiser.png" width = auto height = "100">
                     </a>
-                    <a href="https://cdn.discordapp.com/avatars/214486813069410304/a_1ca389238d70a1dfc1ec2a82d2c54adc.gif?size=1024" data-lightbox="Boosters">
-                        <img src="https://cdn.discordapp.com/avatars/214486813069410304/a_1ca389238d70a1dfc1ec2a82d2c54adc.gif?size=1024" width = auto height = "100">
+                    <a href="Images/Boosters/nikí.png" data-lightbox="Boosters" data-title="Niki">
+                        <img src="Images/Boosters/nikí.png" width = auto height = "100">
                     </a>
-                    <a href="https://cdn.discordapp.com/avatars/817487766778150983/da67d9f0092472196eed34f5d1d8a072.png?size=1024" data-lightbox="Boosters">
-                        <img src="https://cdn.discordapp.com/avatars/817487766778150983/da67d9f0092472196eed34f5d1d8a072.png?size=1024" width = auto height = "100">
+                    <a href="Images/Boosters/Ouchyč.gif" data-lightbox="Boosters" data-title="Ouchyc">
+                        <img src="Images/Boosters/Ouchyč.gif" width = auto height = "100">
                     </a>
-                    <a href="https://cdn.discordapp.com/avatars/544046014282203137/93b402535bdeb7f19860bd62cec85370.png?size=1024" data-lightbox="Boosters">
-                        <img src="https://cdn.discordapp.com/avatars/544046014282203137/93b402535bdeb7f19860bd62cec85370.png?size=1024" width = auto height = "100">
+                    <a href="Images/Boosters/Paloid.png" data-lightbox="Boosters" data-title="Paloid">
+                        <img src="Images/Boosters/Paloid.png" width = auto height = "100">
                     </a>
-                    <a href="https://cdn.discordapp.com/avatars/395985012866023425/5ada6383d22594fd22d5689de43d45ba.png?size=1024" data-lightbox="Boosters">
-                        <img src="https://cdn.discordapp.com/avatars/395985012866023425/5ada6383d22594fd22d5689de43d45ba.png?size=1024" width = auto height = "100">
+                    <a href="Images/Boosters/Shinda Yunikón.png" data-lightbox="Boosters" data-title="Shinda Yunikon">
+                        <img src="Images/Boosters/Shinda Yunikón.png" width = auto height = "100">
                     </a>
-                    <a href="https://cdn.discordapp.com/avatars/623953982095622204/6cd70bcd4a3e12963acb81a1418a5004.png?size=1024" data-lightbox="Boosters">
-                        <img src="https://cdn.discordapp.com/avatars/623953982095622204/6cd70bcd4a3e12963acb81a1418a5004.png?size=1024" width = auto height = "100">
+                    <a href="Images/Boosters/ToxwaT.png" data-lightbox="Boosters" data-title="Toxwat">
+                        <img src="Images/Boosters/ToxwaT.png" width = auto height = "100">
                     </a>
+                    <a href="Images/Boosters/VardaCZ.png" data-lightbox="Boosters" data-title="VardaCZ">
+                        <img src="Images/Boosters/VardaCZ.png" width = auto height = "100">
+                    </a>
+                    <a href="Images/Boosters/Vesi.png" data-lightbox="Boosters" data-title="Vesi">
+                        <img src="Images/Boosters/Vesi.png" width = auto height = "100">
+                    </a>
+                    
                 </div>
             </div>
         </div>
